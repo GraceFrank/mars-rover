9e2cede9a385990485c39cae9ca8ae4840cd31db
Grace <frankgracey@gmil.com>
2021-06-30 04:26:50 +0100

    input parser

--- inputParser.js
+++ inputParser.js
@@ -9,4 +9,21 @@ function paresFile(filePath) {
   }
 }
 
+function parseInput(input) {
+  const lineEntries = input.split('\n')
+  console.log(lineEntries)
+
+  const plateau = lineEntries[0].split(':')[1].split(' ')
+  const rovers = []
+
+  for (let i = 1; i < lineEntries.length; i += 2) {
+    const [name, position] = lineEntries[i].split(':')
+    const [x, y, direction] = position.split(' ')
+    const instruction = lineEntries[i + 1].split(':')[1]
+
+    rovers.push({ x, y, direction, instruction, name: name.split(' ')[0] })
+  }
+  return { plateau, rovers }
+}
+
 module.exports = { paresFile, parseInput }
